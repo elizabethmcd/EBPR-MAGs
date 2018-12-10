bc1c3119d099878f3045043a81c3df56d3dba9b2
Elizabeth McDaniel <emcdaniel@wisc.edu>
2018-12-10 15:16:27 -0600

    need more space, filenames for qc trans

--- executables/QC-and-sortRNA.sh
+++ executables/QC-and-sortRNA.sh
@@ -1,8 +1,6 @@
 #! /bin/bash 
 
-# setup files
-cp $1 .
-tran=$(basename $1)
+ etup filescp $1 tran=$(basename $1)
 file=$(basename $1 .tar.gz)
 name=$(basename $1 .fastq.tar.gz)
 tar -xzf $tran
@@ -22,7 +20,7 @@ cd sortmerna-2.1b
 ./indexdb_rna --ref ./rRNA_databases/silva-bac-16s-id90.fasta,./index/silva-bac-16s-db:\./rRNA_databases/silva-bac-23s-id98.fasta,./index/silva-bac-23s-db:./rRNA_databases/silva-arc-16s-id95.fasta,./index/silva-arc-16s-db:./rRNA_databases/silva-arc-23s-id98.fasta,./index/silva-arc-23s-db:./rRNA_databases/silva-euk-18s-id95.fasta,./index/silva-euk-18s-db:./rRNA_databases/silva-euk-28s-id98.fasta,./index/silva-euk-28s:./rRNA_databases/rfam-5s-database-id98.fasta,./index/rfam-5s-db:./rRNA_databases/rfam-5.8s-database-id98.fasta,./index/rfam-5.8s-db
 
 # Get mRNA 
-./sortmerna --ref ./rRNA_databases/silva-bac-16s-id90.fasta,./index/silva-bac-16s-db:./rRNA_databases/silva-bac-23s-id98.fasta,./index/silva-bac-23s-db:./rRNA_databases/silva-arc-16s-id95.fasta,./index/silva-arc-16s-db:./rRNA_databases/silva-arc-23s-id98.fasta,./index/silva-arc-23s-db:./rRNA_databases/silva-euk-18s-id95.fasta,./index/silva-euk-18s-db:./rRNA_databases/silva-euk-28s-id98.fasta,./index/silva-euk-28s:./rRNA_databases/rfam-5s-database-id98.fasta,./index/rfam-5s-db:./rRNA_databases/rfam-5.8s-database-id98.fasta,./index/rfam-5.8s-db --reads ../$name.qced.fastq  --fastx --aligned ../$name-rRNA.qced.fastq --other ../$name-nonrRNA.qced.fastq --log -v -m 1 -a 14
+./sortmerna --ref ./rRNA_databases/silva-bac-16s-id90.fasta,./index/silva-bac-16s-db:./rRNA_databases/silva-bac-23s-id98.fasta,./index/silva-bac-23s-db:./rRNA_databases/silva-arc-16s-id95.fasta,./index/silva-arc-16s-db:./rRNA_databases/silva-arc-23s-id98.fasta,./index/silva-arc-23s-db:./rRNA_databases/silva-euk-18s-id95.fasta,./index/silva-euk-18s-db:./rRNA_databases/silva-euk-28s-id98.fasta,./index/silva-euk-28s:./rRNA_databases/rfam-5s-database-id98.fasta,./index/rfam-5s-db:./rRNA_databases/rfam-5.8s-database-id98.fasta,./index/rfam-5.8s-db --reads ../$name.qced.fastq  --fastx --aligned ../$name-rRNA.qced --other ../$name-nonrRNA.qced --log -v -m 1 -a 14
 
 # copy back to gluster
 tar -czvf $name-nonrRNA.qced.fastq $name-nonrRNA.qced.fastq.tar.gz
