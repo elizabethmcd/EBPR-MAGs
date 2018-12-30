243a44158e858f8c903bee37b10d8e6b308535e1
Elizabeth McDaniel <emcdaniel@wisc.edu>
2018-12-30 13:39:32 -0500

    reformatting scripts fix fastq files

---
+++ executables/reformatReads.sh
@@ -0,0 +1,21 @@
+#! /bin/bash 
+
+# Reformat fastq files and toss broken reads to not mess up downstream analyses
+
+# File and software setup
+meta=$1
+mkdir trans
+tar -xvzf BBMap_38.07.tar.gz
+cp $1 trans/
+cd trans
+tar -xzf $meta
+cd ..
+metabase=$(basename $meta .tar.gz)
+metarun=$(basename $meta .fastq.tar.gz)
+
+# Reformat reads 
+bbmap/reformat.sh in=trans/metabase out=trans/$metarun.fixed.fastq tossbrokenreads
+
+# Copy back to gluster
+tar -czvf $metarun.fixed.fastq.tar.gz trans/$metarun.fixed.fastq.tar.gz
+cp $metarun.fixed.fastq.tar.gz /mnt/gluster/emcdaniel
