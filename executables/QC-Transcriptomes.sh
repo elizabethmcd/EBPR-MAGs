fea2a43337de101d2c718e52a7a0302dac83485d
Elizabeth McDaniel <emcdaniel@wisc.edu>
2018-12-30 20:35:20 -0500

    bbduk for filtering metatranscriptomes

--- executables/QC-Transcriptomes.sh
+++ executables/QC-Transcriptomes.sh
@@ -1,20 +1,25 @@
 #! /bin/bash 
 
-# setup files
-cp $1 .
-tar -xzvf *.gz 
-file=$(basename $1 .tar.gz)
-name=$(basename $1 .fastq.tar.gz)
+# Metatranscriptome variable
+meta=$1
+mkdir trans
 
-# software setup
-wget http://opengene.org/fastp/fastp
-chmod a+x ./fastp
+# Programs
+tar -xvzf BBMap_38.07.tar.gz
+
+# Copy over metatranscriptomic read files from Gluster and decompress
+cp $1 trans/
+cd metagenomes
+tar -xzf $meta
+cd ..
+metabase=$(basename $1 .tar.gz)
+metarun=$(basename $metabase .fastq.tar.gz)
 
 # QC 
-./fastp -i $file -o $name.qced.fastq --cut_by_quality3
+bbmap/bbduk.sh in=trans/$metabase out=trans/$metarun.bbduk.qced.fastq qtrim=r trimq=10 maq=10
 
 # copy back to gluster
-tar -czvf $name.qced.fastq.tar.gz $name.qced.fastq
-cp $name.qced.fastq.tar.gz /mnt/gluster/emcdaniel/
+tar -czvf $metarun.bbduk.qced.fastq.tar.gz $metarun.bbduk.qced.fastq
+cp $metarun.bbduk.qced.fastq.tar.gz /mnt/gluster/emcdaniel/
 
 
