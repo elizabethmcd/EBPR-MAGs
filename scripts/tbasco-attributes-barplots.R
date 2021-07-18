cc59fa40747f6ae620337b236af9cbb5b4d6183d
Elizabeth McDaniel <emcdaniel@wisc.edu>
2021-07-17 21:14:18 -0500

    network and barplot figures

--- scripts/tbasco-attributes-barplots.R
+++ scripts/tbasco-attributes-barplots.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(RColorBrewer)
 
 # counts for all genomes 
 attribute_counts <- read.csv("results/tbasco_results/all_genome_counts.csv")
@@ -31,7 +32,9 @@ lineage_table[is.na(lineage_table)] <- "Other Categories"
 lineage.labs <- c("Actinobacteria (n=10)", "Alphaproteobacteria (n=15)", "Bacteroidetes (n=9)", "Betaproteobacteria (n=9)", "Gammaproteobacteria (n=4)")
 names(lineage.labs) <- c("actinobacteria", "alphaproteobacteria", "bacteroidetes", "betaproteobacteria", "gammaproteobacteria")
 
-lineage_attributes_plot <- lineage_table %>% ggplot(aes(x=factor(Genomes), fill=General_category)) + geom_bar() + facet_wrap(~ filename, labeller = labeller(filename=lineage.labs), nrow=1) + scale_y_continuous(expand=c(0,0), limits=c(0, 700), breaks=seq(0,700,100)) + scale_fill_brewer(palette="Set3") + labs(fill = "KEGG Module Category") + xlab("# of Genomes with Attribute") + ylab("Count") + theme_bw() + theme(legend.position="bottom")
+lineage_attributes_plot <- lineage_table %>% ggplot(aes(x=factor(Genomes), fill=General_category)) + geom_bar() + facet_wrap(~ filename, scales=c("free_x"), labeller = labeller(filename=lineage.labs), nrow=1) + scale_y_continuous(expand=c(0,0), limits=c(0, 700), breaks=seq(0,700,100)) + scale_fill_brewer(palette="Set3") + labs(fill = "KEGG Module Category") + xlab("# of Genomes with Attribute") + ylab("Count") + theme_bw() + theme(legend.position="bottom")
 
 ggsave("figs/all-genomes-attributes-barplot.png", all_genomes_attribute_barplot, width=13, height=6, units=c("in"))
 ggsave("figs/lineage-attributes-barplot.png", lineage_attributes_plot, width=13, height=4, units=c("in"))
+
+brewer.pal(10, "Set3")
